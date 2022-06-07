f2c2a5c79c6f8256bcfe0f5b59ca463280a4ed75
Alex Paris <alex_paris28@hotmail.com>
2022-06-07 20:02:57 -0300

    feat: csv-parse and stream file

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,6 +1,21 @@
+import fs from 'fs';
+import { parse } from 'csv-parse';
+
 class ImportCategoryUseCase {
   execute(file: Express.Multer.File | undefined) {
-    console.log(file);
+    if (!file) {
+      throw new Error('Not a valid file!');
+    }
+
+    const stream = fs.createReadStream(file.path);
+
+    const parseFile = parse();
+
+    stream.pipe(parseFile);
+
+    parseFile.on('data', async line => {
+      console.log(line);
+    });
   }
 }
 
