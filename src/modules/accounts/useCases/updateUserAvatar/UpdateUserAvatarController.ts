2cc2617ea524ec9f9ad7344ded6dd30284e250e2
Alex Paris <alex_paris28@hotmail.com>
2022-06-19 09:33:21 -0300

    chore: user avatar initial

---
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import { AppError } from '../../../../errors/AppError';
+
+import UpdateUserAvatarUseCase from './UpdateUserAvatarUseCase';
+
+class UpdateUserAvatarController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { id: user_id } = request.user;
+    const avatar_file = request.file?.filename;
+
+    if (!avatar_file) {
+      throw new AppError('File not found!');
+    }
+
+    const updateUserAvatarUseCase = container.resolve(UpdateUserAvatarUseCase);
+
+    await updateUserAvatarUseCase.execute({ user_id, avatar_file });
+
+    return response.status(204).send();
+  }
+}
+
+export default UpdateUserAvatarController;
