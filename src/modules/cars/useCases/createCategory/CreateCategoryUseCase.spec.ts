3ffe2c9253fe863fc55e73bfeef15b9faea56257
Alex Paris <alex_paris28@hotmail.com>
2022-06-19 19:36:50 -0300

    feat: jest and first spec of CreateCategoryService

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,41 @@
+import { AppError } from '../../../../errors/AppError';
+import FakeCategoriesRepository from '../../repositories/fakes/FakeCategoriesRepository';
+import CreateCategoryUseCase from './CreateCategoryUseCase';
+
+let fakeCategoriesRepository: FakeCategoriesRepository;
+let createCategoryUseCase: CreateCategoryUseCase;
+
+describe('Create Category', () => {
+  beforeEach(() => {
+    fakeCategoriesRepository = new FakeCategoriesRepository();
+    createCategoryUseCase = new CreateCategoryUseCase(fakeCategoriesRepository);
+  });
+
+  it('should be able to create a new category', async () => {
+    const category = {
+      name: 'Category Test',
+      description: 'Category description test',
+    };
+
+    await createCategoryUseCase.execute(category);
+
+    const createdCategory = await fakeCategoriesRepository.findByName(
+      category.name
+    );
+
+    expect(createdCategory).toHaveProperty('id');
+  });
+
+  it('should not be able to create a new category with same name', async () => {
+    expect(async () => {
+      const category = {
+        name: 'Category Test',
+        description: 'Category description test',
+      };
+
+      await createCategoryUseCase.execute(category);
+
+      await createCategoryUseCase.execute(category);
+    }).rejects.toBeInstanceOf(AppError);
+  });
+});
