10a5f2827692030381d708c4c6b3e2b51460fbe3
Alex Paris <alex_paris28@hotmail.com>
2022-06-20 22:12:02 -0300

    chore: Car entitie and repo implementation

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,8 +1,10 @@
 import { container } from 'tsyringe';
 
+import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
 import { IUsersRepository } from '../../modules/accounts/repositories/IUsersRepository';
 import { ICategoriesRepository } from '../../modules/cars/repositories/ICategoriesRepository';
 import { ISpecificationRepository } from '../../modules/cars/repositories/ISpecificationsRepository';
+import CarsRepository from '@modules/cars/infra/typeorm/repositories/CarsRepository';
 import UsersRepository from '@modules/accounts/infra/typeorm/repositories/UsersRepository';
 import CategoriesRepository from '@modules/cars/infra/typeorm/repositories/CategoriesRepository';
 import SpecificationsRepository from '@modules/cars/infra/typeorm/repositories/SpecificationsRepository';
@@ -21,3 +23,5 @@ container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   UsersRepository
 );
+
+container.registerSingleton<ICarsRepository>('CarsRepository', CarsRepository);
