c1ae4a9195878ec2fcc0327d9f47108ffdd59caa
Alex Paris <alex_paris28@hotmail.com>
2022-06-20 22:44:59 -0300

    chore: seed file and connection adapt

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -4,7 +4,7 @@ import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 import swaggerUi from 'swagger-ui-express';
 
-import '@shared/infra/typeorm';
+import createConnection from '@shared/infra/typeorm';
 
 import '@shared/container';
 
@@ -12,6 +12,7 @@ import router from './routes';
 import swaggerFile from '../../../swagger.json';
 import { AppError } from '@errors/AppError';
 
+createConnection();
 const app = express();
 
 app.use(express.json());
@@ -20,6 +21,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   if (err instanceof AppError) {
     return response.status(err.statusCode).json({
