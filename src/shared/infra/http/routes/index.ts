40668d2e22a1f6c087bd959d9b704e3907e9ba56
Alex Paris <alex_paris28@hotmail.com>
2022-06-20 22:12:40 -0300

    chore: createCar services and route

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 
+import carsRoutes from './cars.routes';
 import usersRoutes from './users.routes';
 import categoriesRoutes from './categories.routes';
 import authenticateRoutes from './authenticate.routes';
@@ -8,6 +9,7 @@ import specificationsRoutes from './specifications.routes';
 const router = Router();
 
 router.use(authenticateRoutes);
+router.use('/cars', carsRoutes);
 router.use('/users', usersRoutes);
 router.use('/categories', categoriesRoutes);
 router.use('/specifications', specificationsRoutes);
