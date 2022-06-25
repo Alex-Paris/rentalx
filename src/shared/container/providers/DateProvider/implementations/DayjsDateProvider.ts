4a5ae5486d91d48be9082c48feec411c5efb4268
Alex Paris <alex_paris28@hotmail.com>
2022-06-25 15:49:29 -0300

    feat: dayJs provider and adaption

---
+++ src/shared/container/providers/DateProvider/implementations/DayjsDateProvider.ts
@@ -0,0 +1,23 @@
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+
+import { IDateProvider } from '../IDateProvider';
+
+dayjs.extend(utc);
+
+export class DayjsDateProvider implements IDateProvider {
+  compareInHours(start_date: Date, end_date: Date): number {
+    const startDateFormatted = this.convertToUTC(start_date);
+    const endDateFormatted = this.convertToUTC(end_date);
+
+    return dayjs(endDateFormatted).diff(startDateFormatted, 'hours');
+  }
+
+  convertToUTC(date: Date): string {
+    return dayjs(date).utc().local().format();
+  }
+
+  dateNow(): Date {
+    return dayjs().toDate();
+  }
+}
