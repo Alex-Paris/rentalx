5b3d0ea302a04d816040575194d6ef8bf2f1ee07
Alex Paris <alex_paris28@hotmail.com>
2022-06-26 09:31:48 -0300

    feat: supertest and test in controllers

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -0,0 +1,70 @@
+import { Connection } from 'typeorm';
+import request from 'supertest';
+import { hash } from 'bcryptjs';
+import { v4 as uuidV4 } from 'uuid';
+
+import { app } from '@shared/infra/http/app';
+import createConnection from '@shared/infra/typeorm';
+
+let connection: Connection;
+describe('Create Category Controller', () => {
+  beforeAll(async () => {
+    connection = await createConnection();
+    await connection.runMigrations();
+
+    const id = uuidV4();
+    const password = await hash('admin', 8);
+
+    await connection.query(
+      `INSERT INTO users(id, name, email, password, "isAdmin", driver_license, created_at) 
+      VALUES ('${id}', 'admin', 'admin@sample.com', '${password}', true, '000123', 'now()')`
+    );
+  });
+
+  afterAll(async () => {
+    await connection.dropDatabase();
+    await connection.close();
+  });
+
+  it('should be able to create a new category', async () => {
+    const responseToken = await request(app).post('/sessions').send({
+      email: 'admin@sample.com',
+      password: 'admin',
+    });
+
+    const { token } = responseToken.body;
+
+    const response = await request(app)
+      .post('/categories')
+      .send({
+        name: 'Category sample',
+        description: 'Category sample description',
+      })
+      .set({
+        Authorization: `Bearer ${token}`,
+      });
+
+    expect(response.status).toBe(201);
+  });
+
+  it('should not be able to create a new category with name exists', async () => {
+    const responseToken = await request(app).post('/sessions').send({
+      email: 'admin@sample.com',
+      password: 'admin',
+    });
+
+    const { token } = responseToken.body;
+
+    const response = await request(app)
+      .post('/categories')
+      .send({
+        name: 'Category sample',
+        description: 'Category sample description',
+      })
+      .set({
+        Authorization: `Bearer ${token}`,
+      });
+
+    expect(response.status).toBe(400);
+  });
+});
